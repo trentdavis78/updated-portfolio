b8cd74be4930e6596d781d6f8661aa01efec7da2
Trent Davis <trentdavisinc@gmail.com>
2019-02-18 17:58:05 -0600

    Added Materialize CSS, general layout, some home page stuff

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,66 @@
-$(document).ready(function(){
+(function($) {
 
-});
+    /**
+     * Copyright 2012, Digital Fusion
+     * Licensed under the MIT license.
+     * http://teamdf.com/jquery-plugins/license/
+     *
+     * @author Sam Sehnert
+     * @desc A small plugin that checks whether elements are within
+     *     the user visible viewport of a web browser.
+     *     only accounts for vertical position, not horizontal.
+     */
+  
+    $.fn.visible = function(partial) {
+      
+        var $t            = $(this),
+            $w            = $(window),
+            viewTop       = $w.scrollTop(),
+            viewBottom    = viewTop + $w.height(),
+            _top          = $t.offset().top,
+            _bottom       = _top + $t.height(),
+            compareTop    = partial === true ? _bottom : _top,
+            compareBottom = partial === true ? _top : _bottom;
+      
+      return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+  
+    };
+      
+  })(jQuery);
+
+  $(document).ready(function() {
+
+    $(".button-collapse").sideNav();
+    $(window).scroll(function(event) {
+  
+        $(".module").each(function(i, el) {
+          var el = $(el);
+          if (el.visible(true)) {
+            el.addClass("come-in"); 
+          } 
+        });
+        
+      });
+      $("#phoneToggle").on("click", function(){
+          if($(this).attr("data-visibility") == "hidden"){
+            $("#phoneNumber").text("512.938.9851");
+            $(this).attr("data-visibility", "visible");
+            $("#phoneToggle").text("Hide");
+          } else {
+            $("#phoneNumber").text("512.*******");
+            $(this).attr("data-visibility", "hidden");
+            $("#phoneToggle").text("Show");
+          }
+      });
+      $("#emailToggle").on("click", function(){
+        if($(this).attr("data-visibility") == "hidden"){
+          $("#emailAddress").html("<a href='mailto:trentdavisinc@gmail.com'>trentdavisinc@gmail.com</a>");
+          $(this).attr("data-visibility", "visible");
+          $("#emailToggle").text("Hide");
+        } else {
+          $("#emailAddress").text("*************@gmail.com");
+          $(this).attr("data-visibility", "hidden");
+          $("#emailToggle").text("Show");
+        }
+    });
+  });
