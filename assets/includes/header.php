51fa5bff3087937c26c142bd2688841631382fcb
Trent Davis <trentdavisinc@gmail.com>
2019-02-18 20:15:03 -0600

    Updated to PHP

---
+++ assets/includes/header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>TDWeb.Dev</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Meyer CSS reset-->
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/reset.css" />
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Compiled and minified CSS/JS for Materialize -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+        integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/style.css" />
+    <!-- DEVICON and GOOGLE FONTS -->
+    <link rel="stylesheet"
+        href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Permanent+Marker|Varela+Round" rel="stylesheet">
+
+</head>
+
+<body>
+    <!-- MOBILE RESPONSIVE MENU -->
+    <header>
+        <nav>
+            <div class="nav-wrapper teal darken-3">
+                <div class="container">
+                    <a href="#!" class="brand-logo">
+                        <img src="assets/images/logo.svg" class="responsive-img">
+                        <small
+                            style="position:relative;left:13px;font-family: 'Permanent Marker', cursive;">Web.dev</small></a>
+                    <a href="#" data-activates="mobile-demo" class="button-collapse"><i
+                            class="material-icons">menu</i></a>
+                    <ul class="right hide-on-med-and-down">
+                        <li class="active"><a href="#!">Home</a></li>
+                        <li><a href="#!">Portfolio</a></li>
+                        <li><a href="#!">About</a></li>
+                        <li><a href="#!">Services</a></li>
+                        <li><a href="#!">Resources</a></li>
+                        <li><a href="#!">Contact</a></li>
+                    </ul>
+                    <ul class="side-nav" id="mobile-demo">
+                        <li><a href="#!">Home</a></li>
+                        <li><a href="#!">Portfolio</a></li>
+                        <li><a href="#!">About</a></li>
+                        <li><a href="#!">Services</a></li>
+                        <li><a href="#!">Resources</a></li>
+                        <li><a href="#!">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>    
+    <main>
